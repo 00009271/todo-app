dc0854c1b73e8baf58e6fc552345c208f59df352
00009271 <akbarkhanabdukharimov2002@gmail.com>
2021-04-07 15:31:57 +0500

    'beginning'

---
+++ index.js
@@ -0,0 +1,89 @@
+//libraries
+const { constants } = require('buffer')
+const express = require('express')
+const app = express()
+const fs = require('fs')
+
+const DB = './database/db.json'
+
+//view engine
+app.set('view engine', 'pug')
+//static styles
+app.use('/static', express.static('public'))
+//using express url encoder
+app.use(express.urlencoded({ extended: false }))
+//runing the app on port:
+app.listen(9271, (err) => {
+    if (err) throw err
+    console.log(` ToDo App runes on port 9271 http://localhost:9271`)
+})
+//id generator
+function id () {
+    return '_' + Math.random().toString(36).substr(2, 9);
+}
+//geting tasks from db
+app.get('/', (req, res) => {
+    fs.readFile(DB, (err, data) => {
+      if (err) throw err
+      const tasks = JSON.parse(data)
+      res.render('index', { tasks: tasks })
+    })
+})
+//sending user input to db
+app.post('/create', (req, res) => {
+    const form_data = req.body
+  
+    if (form_data.task.trim() == '') {
+      fs.readFile(DB, (err, data) => {
+        if (err) throw err
+  
+        const tasks = JSON.parse(data)
+  
+        res.render('index', { error: true, tasks: tasks })
+      })
+    } else {
+      fs.readFile(DB, (err, data) => {
+        if (err) throw err
+  
+        const tasks = JSON.parse(data)
+  
+        const task = {
+          id: id(),
+          description: form_data.task,
+          done: false
+        }
+  
+        tasks.push(task)
+  
+        fs.writeFile(DB, JSON.stringify(tasks), (err) => {
+          if (err) throw err
+  
+          fs.readFile(DB, (err, data) => {
+            if (err) throw err
+  
+            const tasks = JSON.parse(data)//tasks=>tasks
+  
+            res.render('index', { success: true, tasks: tasks })
+          })
+        })
+      })
+    }
+})
+//deleting the task
+app.get('/:id/delete', (req, res) => {
+    const id = req.params.id
+  
+    fs.readFile(DB, (err, data) => {
+      if (err) throw err
+  
+      const tasks = JSON.parse(data)
+  
+      const filteredtasks = tasks.filter(todo => todo.id != id)
+  
+      fs.writeFile(DB, JSON.stringify(filteredtasks), (err) => {
+        if (err) throw err
+  
+        res.render('index', { tasks: filteredtasks, deleted: true })
+      })
+    })
+})
